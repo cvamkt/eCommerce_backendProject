192e5184ec164e04ce69ab21ae547ea91cb5d0c1
cvamkt <shivamkrthakur9771@gmail.com>
2025-04-12 13:25:54 +0530

    logIn_API

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,8 @@
  */
 const bcrypt = require('bcryptjs')
 const user_model = require("../models/user.model")
+const jwt = require('jsonwebtoken')
+const secret = require("../Config/auth.config")
 
 
 exports.signup = async (req, res) => {
@@ -40,4 +42,42 @@ exports.signup = async (req, res) => {
 
 
     //3) Return the response back to the user
+}
+
+//_____________________________________________________________________________________
+
+
+exports.signIn = async (req, res) => {
+
+    // check if the userId n password is correct or not
+    const user = await user_model.findOne({ userId: req.body.userId })
+    if (user == null) {
+        return res.status(400).send({
+            message: "USer id passed is not valid"
+        })
+    }
+
+    const isPasswordValid = bcrypt.compareSync(req.body.password, user.password)
+    if (!isPasswordValid) {
+        return res.status(401).send({
+            message: "wrong password !"
+        })
+    }
+
+
+    // using jwt we will create the access tokem with a given token(TTL) nad return
+
+    const token = jwt.sign({ id: user.userId }, secret.secret, {
+        expiresIn: 120 //second
+    })
+
+    res.status(200).send({
+        user: user.name,
+        userId: user.userId,
+        email: user.email,
+        userType: user.userType,
+        accessTokem: token
+
+    })
+
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,4 +7,16 @@
 
  module.exports = (app)=>{
     app.post("/api/v1/auth/signup",[authMiddleware.verifySignUpBody], authController.signup)
+
+
+    
+    /**
+     * route for
+     * POST localhost:8888/api/v1/auth/signIn
+     */
+
+    app.post("/api/v1/auth/signIn",authController.signIn)
  }
+
+
+
